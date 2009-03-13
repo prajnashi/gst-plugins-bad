aedde35605b9a51d530e553467ebbc280ee830aa
Edward Hervey <bilboed@bilboed.com>
2009-03-13 18:12:13 +0100

    cleanup makefiles. No longer requires building in a subdirectory

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-GSTREAMER_TOP := $(LOCAL_PATH)
+GST_PLUGINS_BAD_TOP := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
-include $(GSTREAMER_TOP)/sys/fbdev/Android.mk
+include $(GST_PLUGINS_BAD_TOP)/sys/fbdev/Android.mk
 
